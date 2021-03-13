8b6826c049c6aa6d69db9cd5fee2fc88cec0f5b8
Adama Diaby <shalomservices59@gmail.com>
2021-03-13 11:38:16 -0500

    updated dropdown

--- src/components/dropdown/dropdown.component.jsx
+++ src/components/dropdown/dropdown.component.jsx
@@ -4,6 +4,8 @@ import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { selectCartItems } from "../../redux/cart/cart.selectors";
 
+import Button from "../button/button.component";
+
 const Dropdown = ({ cartItems }) => {
   return (
     <div className="dropdown">
@@ -11,9 +13,20 @@ const Dropdown = ({ cartItems }) => {
         <p className="dropdown__empty-message">No cart item !</p>
       ) : (
         <div className="dropdown__items">
-            {
-                cartItems.map( item => <div key={item.id} > {`${item.name} --> ${item.quantity}`} </div> )
-            }
+          {cartItems.map((item) => (
+            <div className="dropdown__item" key={item.id}>
+              <img
+                src={item.imageUrl}
+                alt=""
+                className="dropdown__item-image"
+              />
+              <p className="dropdown__item-name"> {item.name} </p>
+              <p className="dropdown__item-quantity"> {`x${item.quantity}`} </p>
+            </div>
+          ))}
+          <div className="checkout-box">
+          <Button variant="primary">Checkout</Button>
+          </div>
         </div>
       )}
     </div>
